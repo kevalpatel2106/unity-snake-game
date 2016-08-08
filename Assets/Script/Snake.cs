7a73f63b83236cfc92574685c5c697be89777f75
kevalpatel2106 <kevalpatel2106@gmail.com>
2016-08-08 22:29:04 +0530

    Creating game

---
+++ Assets/Script/Snake.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+	// Did the snake eat something?
+	bool ate = false;
+
+	//Did user died?
+	bool isDied = false;
+
+	// Tail Prefab
+	public GameObject tailPrefab;
+
+	// Current Movement Direction
+	// (by default it moves to the right)
+	Vector2 dir = Vector2.right;
+
+	// Keep Track of Tail
+	List<Transform> tail = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+		// Move the Snake every 300ms
+		InvokeRepeating("Move", 0.3f, 0.3f); 
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!isDied) {
+			// Move in a new Direction?
+			if (Input.GetKey (KeyCode.RightArrow))
+				dir = Vector2.right;
+			else if (Input.GetKey (KeyCode.DownArrow))
+				dir = -Vector2.up;    // '-up' means 'down'
+			else if (Input.GetKey (KeyCode.LeftArrow))
+				dir = -Vector2.right; // '-right' means 'left'
+			else if (Input.GetKey (KeyCode.UpArrow))
+				dir = Vector2.up;
+		} else {
+			if (Input.GetKey(KeyCode.R)){
+				//clear the tail
+				tail.Clear();
+
+				//reset to origin
+				transform.position = new Vector3(0, 0, 0);
+
+				//make snake alive
+				isDied = false;
+			}
+		}
+	}
+
+	void Move() {
+		if (!isDied) {
+			// Save current position (gap will be here)
+			Vector2 v = transform.position;
+
+			// Move head into new direction (now there is a gap)
+			transform.Translate (dir);
+
+			// Ate something? Then insert new Element into gap
+			if (ate) {
+				// Load Prefab into the world
+				GameObject g = (GameObject)Instantiate (tailPrefab,
+					              v,
+					              Quaternion.identity);
+
+				// Keep track of it in our tail list
+				tail.Insert (0, g.transform);
+
+				// Reset the flag
+				ate = false;
+			} else if (tail.Count > 0) {	// Do we have a Tail?
+					// Move last Tail Element to where the Head was
+					tail.Last ().position = v;
+
+					// Add to front of list, remove from the back
+					tail.Insert (0, tail.Last ());
+					tail.RemoveAt (tail.Count - 1);
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		// Food?
+		if (coll.name.StartsWith("Food")) {
+			// Get longer in next Move call
+			ate = true;
+
+			// Remove the Food
+			Destroy(coll.gameObject);
+		} else { 	// Collided with Tail or Border
+			isDied = true;
+		}
+	}
+}
+ 
---
+++ Assets/Script/SpawnFood.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+    // Food Prefab
+    public GameObject foodPrefab;
+
+    // Borders
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    // Use this for initialization
+    void Start () {
+        // Spawn food every 4 seconds, starting in 3
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    // Spawn one piece of food
+    void Spawn() {
+        // x position between left & right border
+        int x = (int)Random.Range(borderLeft.position.x,
+                                  borderRight.position.x);
+
+        // y position between top & bottom border
+        int y = (int)Random.Range(borderBottom.position.y,
+                                  borderTop.position.y);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab,
+                    new Vector2(x, y),
+                    Quaternion.identity); // default rotation
+    }
+}
